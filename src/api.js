03c13ed4ba4060e8c2802f7c18a33fc3408273f2
Blueshadow58 <gamonalfranco@gmail.com>
2022-09-04 03:37:13 -0400

    get all products from API

--- src/api.js
+++ src/api.js
@@ -1,6 +1,40 @@
-const getProducts = (done) => {
-  fetch("https://desafio-bsale-backend-heroku.herokuapp.com/")
-    .then((res) => res.json())
-    .then((data) => done(data))
-    .catch((err) => console.log(err));
+const cardList = document.getElementById("cardList");
+let products = [];
+
+const getProducts = async () => {
+  try {
+    //const res = await fetch("https://api.publicapis.org/entries");
+    const res = await fetch("https://pacific-reaches-62974.herokuapp.com/");
+
+    products = await res.json();
+    makingProducts(products);
+    console.log(products);
+  } catch (err) {
+    console.error(err);
+  }
 };
+
+const makingProducts = (products) => {
+  const html = products
+    ?.map((product) => {
+      return `
+    <div class="col">
+    <div class="card card-height h-100">
+    <div class="img-container h-100">
+    <img src="${product.url_image}" class="card-img "  alt="no img" >
+    </div>
+
+      <div class="card-body">
+        <h6 class="card-title">${product.name}</h6>
+        <span class="card-text h5 text-warning"> $ ${product.price}</span>
+      </div>
+    </div>
+  </div>        
+    `;
+    })
+    .join(" ");
+
+  cardList.innerHTML = html;
+};
+
+getProducts();
